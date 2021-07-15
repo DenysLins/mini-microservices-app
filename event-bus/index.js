8dc59c4afe55ef64f731210a0e655744ef362bf8
Denys André dos Santos Lins <denyslins@gmail.com>
2021-07-15 11:52:10 -0300

    Add event bus

---
+++ event-bus/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/events", async (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+
+  res.send({ status: "ok" });
+});
+
+app.listen(4005, () => {
+  console.log("Event bus listing on port 4005");
+});

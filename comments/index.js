3359f4fa9fa88d6ec78d0207a74a4b085563e3ce
Denys André dos Santos Lins <denyslins@gmail.com>
2021-07-15 11:51:46 -0300

    Update comments

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -13,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[postId] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const postId = req.params.id;
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
@@ -22,9 +23,24 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
   commentsByPostId[postId] = comments;
 
+  const event = {
+    type: "CommentCreated",
+    data: {
+      postId: postId,
+      id: commentId,
+      content: content,
+    },
+  };
+
+  await axios.post("http://localhost:4005/events", event);
+
   res.status(201).send(commentsByPostId[postId]);
 });
 
+app.post("/events", (req, res) => {
+  res.send({ status: "ok" });
+});
+
 app.listen(4001, () => {
-  console.log("Listing on port 4001");
+  console.log("Comments listing on port 4001");
 });

7223cccde38e2ce3f2969934a83c0651fe209ada
Denys André dos Santos Lins <denyslins@gmail.com>
2021-07-15 21:34:20 -0300

    Add moderation

---
+++ moderation/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config({ path: "../.env" });
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const MODERATION_PORT = process.env.MODERATION_PORT;
+const EVENT_BUS_PORT = process.env.EVENT_BUS_PORT;
+const EVENT_BUS_URL = `http://localhost:${EVENT_BUS_PORT}`;
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const { postId, id, content } = data;
+
+    setTimeout(() => {
+      axios.post(`${EVENT_BUS_URL}/events`, {
+        type: "CommentModerated",
+        data: {
+          postId,
+          id,
+          content,
+          status: Math.random() < 0.5 ? "approved" : "denied",
+        },
+      });
+    }, 5000);
+  }
+
+  res.send({ status: "ok" });
+});
+
+app.listen(MODERATION_PORT, () => {
+  console.log(`Moderation listing on port ${MODERATION_PORT}`);
+});

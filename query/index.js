7705f1a2fd58806d86b8e140974aa111bfa9a9e9
Denys André dos Santos Lins <denyslins@gmail.com>
2021-07-15 21:33:56 -0300

    Update query

--- query/index.js
+++ query/index.js
@@ -1,10 +1,12 @@
+require("dotenv").config({ path: "../.env" });
 const express = require("express");
-const { randomBytes } = require("crypto");
 const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const QUERY_PORT = process.env.QUERY_PORT;
 
 const posts = {};
 
@@ -24,16 +26,26 @@ app.post("/events", (req, res) => {
   }
 
   if (type === "CommentCreated") {
-    const { postId, id, content } = data;
+    const { postId, id, content, status } = data;
     posts[postId].comments.push({
       id,
       content,
+      status,
     });
+  }
+
+  if (type === "CommentUpdated") {
+    const { postId, id, contend, status } = data;
+    const commentIndex = posts[postId].comments.findIndex(
+      (comment) => comment.id == id
+    );
+    posts[postId].comments[commentIndex].contend = contend;
+    posts[postId].comments[commentIndex].status = status;
   }
 
   res.send({ status: "ok" });
 });
 
-app.listen(4002, () => {
-  console.log("Query listing on port 4002");
+app.listen(QUERY_PORT, () => {
+  console.log(`Query listing on port ${QUERY_PORT}`);
 });

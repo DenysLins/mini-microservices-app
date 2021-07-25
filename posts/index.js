9185f63310fba5ebea89b9958f2b238e4185d0c0
Denys André dos Santos Lins <denyslins@gmail.com>
2021-07-25 00:06:18 -0300

    Add ingress

--- posts/index.js
+++ posts/index.js
@@ -16,11 +16,7 @@ const EVENT_BUS_URL = `http://event-bus-clusterip-service:${EVENT_BUS_PORT}`;
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   const post = {
